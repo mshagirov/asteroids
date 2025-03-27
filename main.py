554600344ac2914d4a2323840bd8c56fd0d554a1
mshagirov <muratshagirov@gmail.com>
2025-03-27 14:39:25 +0800

    draw Player as a part of a pygame.sprite.Group group

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # object groups
+    updatable = pygame.sprite.Group()
+    drawable  = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x , y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill( (0, 0, 0)) # can use str "black" or (0,0,0)
+        screen.fill( "black" ) # can use str "black" or (0,0,0)
+
+        #player.update(dt)
+        #player.draw(screen)
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
+        
+        # pause the loop for 1/60 sec
+        time_passed = clock.tick(60)
+        dt = time_passed / 1000 # dt in seconds
+
 
 if __name__ == "__main__":
     main()
